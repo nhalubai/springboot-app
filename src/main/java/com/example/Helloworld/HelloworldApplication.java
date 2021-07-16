31ac010cc3a432425951eca2666ef38447bfa865
Ramamurthy <nagaraj.ramamurthy@accenture.com>
2021-07-16 18:22:05 +0530

    My Second commit modfied as war and changes in the main file

--- src/main/java/com/example/Helloworld/HelloworldApplication.java
+++ src/main/java/com/example/Helloworld/HelloworldApplication.java
@@ -2,12 +2,19 @@ package com.example.Helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HelloworldApplication.class);
 	}
 
 }
